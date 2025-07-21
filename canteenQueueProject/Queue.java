4286b95176c5e14d6257986e7ee56ad9b674d158
PikaDisaster2000 <pikadisaste3r2000@gmail.com>
2025-07-21 12:37:39 +1200

    canteenQueueProject Ver 1.0
    A starting point/code for my canteen project. Please note that there may be mistakes/bugs in the code. This is intended to show versioning control.

---
+++ canteenQueueProject/Queue.java
@@ -0,0 +1,50 @@
+
+/**
+ * The class that works the code for 
+ *
+ * Joseph T
+ * Version 1.0
+ */
+public class Queue
+{
+    // instance variables - replace the example below with your own
+    private Node head = null;
+    private Node tail = null;
+    /**
+     * Constructor for objects of class Queue
+     */
+    public Queue()
+    {
+       
+    }
+    
+    public void enqueue(Node node) {
+        if (queueEmpty()) {
+        this.head = node;
+        this.tail = node;
+        } else {
+         this.tail.setNextNode(node);
+         this.tail = node;
+        }
+    }
+    
+    public int dequeue() {
+     Node tempNode = this.head; 
+       if (this.head == this.tail) {
+        this.head = null;
+        this.tail = null;
+     }
+       else {
+        this.head = tempNode.getNextNode();
+     }
+     return(tempNode.getJoinTime());
+    }
+    
+    public boolean queueEmpty() {
+        if (head == null && tail == null) {
+            return(true);
+        } else {
+            return(false);
+        }
+    }
+}
