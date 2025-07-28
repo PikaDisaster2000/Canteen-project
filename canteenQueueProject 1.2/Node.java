071665b084079b63a463ddbae7934b4b1d78918e
PikaDisaster2000 <pikadisaste3r2000@gmail.com>
2025-07-28 15:35:50 +1200

    Canteen Project 1.2
    Version 1.2

---
+++ canteenQueueProject 1.2/Node.java
@@ -0,0 +1,47 @@
+
+/**
+ * The class that sets attributes to for a Node.
+ *
+ * Joseph T
+ * Version 1.2
+ */
+public class Node
+{
+    // instance variables
+    private int joinTime; //the time the student or teacher joined
+    private String type; //the type of person (teacher or student)
+    private Node nextNode;
+    
+    //constructor for nodes with no data
+    public Node(int joinTime)
+    {
+        this.type = "";
+        this.joinTime = joinTime;
+    }
+    //constructor for nodes with data
+    public Node(String type, int joinTime) {
+        this.type = type;
+        this.joinTime = joinTime;
+    }
+    
+    // setters
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setJoinTime(int joinTime) {
+        this.type = type;
+    }
+    public void setNextNode(Node node) {
+        this.nextNode = node;
+    }    
+    //getters 
+    public String getType() {
+        return(this.type);
+    }
+    public int getJoinTime() {
+        return(this.joinTime);
+    }
+    public Node getNextNode() {
+        return(this.nextNode);
+    }
+}
